42a1f6f93737c43fd05a88429ce3fffbd48a83d4
TrigunGurumurti <triguntamragouri@gmail.com>
2024-11-07 18:07:13 +0530

    var Let and Const

---
+++ Basics/varLetConst.js
@@ -0,0 +1,26 @@
+var a = 5;
+console.log(a);
+var a = 10;
+console.log(a);
+
+
+let b = 2;
+console.log(b);
+
+
+// let c = 10;
+// console.log(c);
+// let c = 11;       Error
+// console.log(c);
+
+
+const d = 3;
+console.log(d);
+
+
+// const e = 10;
+// console.log(e);
+// let e = 11;       Error
+// console.log(e);
+
+
