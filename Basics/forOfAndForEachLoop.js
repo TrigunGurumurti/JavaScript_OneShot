1e23ec0c3ca8d5ceb8ab90916e61fc737bc3d7c5
TrigunGurumurti <triguntamragouri@gmail.com>
2024-11-17 00:10:52 +0530

    forOf and forEach Loop

---
+++ Basics/forOfAndForEachLoop.js
@@ -0,0 +1,16 @@
+arr = [1,2,3];
+
+// ForOf Loop
+for(const ele of arr)
+{
+    console.log(ele);  // 1 2 3
+}
+
+
+brr = [4,5,6];
+
+// ForEach Loop
+brr.forEach( (ele,i) =>
+{
+    console.log(ele);  // 4 5 6
+});
