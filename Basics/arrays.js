0dba51b49a06bd2793ad189af57a7257088d0d72
TrigunGurumurti <triguntamragouri@gmail.com>
2024-11-16 23:49:33 +0530

    Basic functions involved in arrays

---
+++ Basics/arrays.js
@@ -0,0 +1,15 @@
+arr = [2,3,4];
+console.log(arr);  // 2 3 4
+
+arr.push(5);
+console.log(arr);  // 2 3 4 5
+
+arr.pop();
+console.log(arr);  // 2 3 4
+
+arr.unshift(1);
+console.log(arr);  // 1 2 3 4
+
+
+arr.shift(1);
+console.log(arr);  // 2 3 4
