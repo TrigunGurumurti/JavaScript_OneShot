12292327b02ad8d470e2efd0bcc2cca602ef1445
TrigunGurumurti <triguntamragouri@gmail.com>
2024-11-16 23:55:25 +0530

    Basic functions involved in arrays

--- Basics/arrays.js
+++ Basics/arrays.js
@@ -10,6 +10,10 @@ console.log(arr);  // 2 3 4
 arr.unshift(1);
 console.log(arr);  // 1 2 3 4
 
-
 arr.shift(1);
 console.log(arr);  // 2 3 4
+
+
+// We can store elements of different data type in arrays
+brr = [1,"trigun",4.5];
+console.log(brr);  // 1 'trigun' 4.5
